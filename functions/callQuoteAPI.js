a48245b042d09ba3a47c4ae61adba4e63a9d5a30
code-with-zen <rajanlakhani@gmail.com>
2021-10-25 16:55:20 -0700

    changed url

--- functions/callQuoteAPI.js
+++ functions/callQuoteAPI.js
@@ -2,12 +2,19 @@ const fetch = require("node-fetch");
 
 const API_ENDPOINT = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
-
+/*
 exports.handler = async (event, context, callback) => {
   return fetch(API_ENDPOINT)
     .then((response) => response.json())
-    .then((data) => ({ statusCode: 200, body: JSON.stringify(data) }))
+    .then((data) => ({ statusCode: 200, body: "JSON.stringify(data)" }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
 
+*/
 
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: "Hello, World"
+  };
+};
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const quoteNetlifyFunctions = "/.netlify/functions/callQuoteAPI.js";
+const quoteNetlifyFunctions =
+  "https://ron-swanson-quotes-api.netlify.app/.netlify/callQuoteAPI";
 
 //call netlify functions for api
 function callQuoteAPI() {
   fetch(quoteNetlifyFunctions)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      console.log(data.body);
     })
     .catch((error) => console.error(error));
 }
