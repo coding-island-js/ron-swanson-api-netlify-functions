3fe37fae91077c9d59cfc13e88b5e4ccaacc5df1
code-with-zen <rajanlakhani@gmail.com>
2021-10-27 20:52:19 -0700

    added type:module in package.json

--- functions/ronSwansonQuotes.js
+++ functions/ronSwansonQuotes.js
@@ -1,10 +1,16 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
 const API_ENDPOINT = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
 exports.handler = async (event, context, callback) => {
+  let obj = {};
+  obj["name"] = "test";
+  obj["test"] = "test12";
   return fetch(API_ENDPOINT)
     .then((res) => res.json())
-    .then((data) => ({ statusCode: 200, body: JSON.stringify(data) }))
+    .then((data) => ({
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
